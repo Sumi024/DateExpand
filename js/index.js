cd8c5733e95b97f21d75d8683f530dd25cedfa33
youjingtian <Suumi024@gmail.com>
2018-10-17 19:39:02 +0800

    增加format

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
+/*
+ * creater : Sumi
+ * time: 2018/10/17
+ */
+
 Date.prototype.toLocalTime = toLocalTime;
 Date.prototype.getTimeZone = getTimeZone;
 Date.prototype.translateTime = translateTime;
 
-function toLocalTime(date){
+function toLocalTime(date,status){
     // Format yyyy-mm-dd hh:mm:ss
     var timeZone = Number(getTimeZone()) * 10 * 60 * 60;
+    if(status){
+        timeZone = - timeZone;
+    }
     date = new Date(Number(date) + timeZone);
     return date.getFullYear().nString(4) + '-' + (date.getMonth() + 1).nString(2) + '-' + date.getDate().nString(2) + ' ' + date.getHours().nString(2) + ':' + date.getMinutes().nString(2) + ':' + date.getSeconds().nString(2);
 }
@@ -15,7 +23,17 @@ function getTimeZone(){
     return time.match(reg)[1];
 }
 
-function translateTime(data){
+function translateTime(date){
     // Format yyyy-mm-dd hh:mm:ss
     return data.replace(/\-+/g, '/');
+}
+
+
+/*
+ * 通过类似注解的形式，处理时间输出的模式
+ * 如：format => yyyy-mm-dd hh-mm-ss , output => 2018-10-17 17:02:03
+ * 默认注解字段: 年 => y, 月 => m, 天 => d, 时 => h, 分 => m, 秒 => s
+ */
+function format(date, format){
+
 }
